834bbe5a8354f530775003b8d1832861200acdc1
Mahmoud Hamad <mahmoud_smgl@hotmail.com>
2026-09-24 23:12:55 +0300

    Add: products-of-array-discluding-self - submission-5

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-5.swift
@@ -0,0 +1,25 @@
+class Solution {
+    func productExceptSelf(_ nums: [Int]) -> [Int] {        
+        var l = [Int](repeating: 0, count: nums.count)
+        l[0] = 1
+        for i in 1 ..< nums.count {
+            let num = nums[i-1] // we don't count current
+            l[i] = l[i-1] * num
+        }
+        //print(l)
+
+        var r = [Int](repeating: 0, count: nums.count)
+        r[nums.count -  1] = 1
+        for i in stride(from: nums.count - 2, to: -1, by: -1) {
+            let num = nums[i+1] // we don't count current
+            r[i] = r[i+1] * num
+        }
+        //print(r)
+
+        var ans = [Int](repeating: 0, count: nums.count)
+        for i in 0 ..< nums.count {
+            ans[i] = l[i] * r[i] 
+        }
+        return ans
+    }
+}
