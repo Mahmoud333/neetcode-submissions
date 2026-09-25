722d9c77b1a7d1b96ef177f18f95184cfd167306
Mahmoud Hamad <mahmoud_smgl@hotmail.com>
2026-09-26 00:55:06 +0300

    Add: surrounded-regions - submission-2

---
+++ Data Structures & Algorithms/surrounded-regions/submission-2.swift
@@ -0,0 +1,45 @@
+class Solution {
+    func solve(_ board: inout [[Character]]) {
+        var v = Set<[Int]>() //visited
+        var tv = Set<[Int]>() //temp visited
+        
+        func dfs(_ r: Int, _ c: Int) -> Bool {
+            if r < 0 || r > board.count - 1 { return false }
+            if c < 0 || c > board[0].count - 1 { return false }
+            if board[r][c] == "X" { return true }
+            //if v.contains("\(r),\(c)") { return false }
+            if tv.contains([r,c]) { return false }
+            board[r][c] = "X"
+            //v.insert("\(r),\(c)")
+            tv.insert([r,c])
+            let surrounded = dfs(r - 1, c) && dfs(r + 1, c) && dfs(r, c-1) && dfs(r, c+1)
+            if surrounded {
+                return true
+            } else {
+                board[r][c] = "O"
+                return false
+            }
+        }
+
+        for r in 0 ..< board.count {
+            for c in 0 ..< board[0].count {
+                tv = Set<[Int]>()
+                if board[r][c] == "O" {
+                    if dfs(r, c) == false {
+                        for v in tv {
+                            board[v[0]][v[1]] = "O"
+                        }
+                    }
+                    v = v.union(tv)
+                }
+            }
+        }
+    }
+}
+/*
+"X","O","X","O","X","O","O","O","X","O"],["X","O","O","X","X","X","O","O","O","X"],["O","O","O","O","O","O","O","O","X","X"],["O","O","O","O","O","O","X","O","O","X"],["O","O","X","X","X","X","X","O","O","O"],["X","O","O","X","X","X","X","X","X","O"],["X","O","X","X","X","X","X","O","X","O"],["X","X","O","X","X","X","X","O","O","X"],["O","O","O","O","X","X","X","O","X","O"],["X","X","O","X","X","X","X","O","O","O"]]
+---------------------------------------------
+"X","O","X","O","X","O","O","O","X","O"],["X","O","O","X","X","X","O","O","O","X"],["O","O","O","O","O","O","O","O","X","X"],["O","O","O","O","O","O","X","O","O","X"],["O","O","X","X","O","X","X","O","O","O"],["X","O","O","X","X","X","X","X","X","O"],["X","O","X","X","X","X","X","O","X","O"],["X","X","O","X","X","X","X","O","O","X"],["O","O","O","O","X","X","X","O","X","O"],["X","X","O","X","X","X","X","O","O","O"]]
+
+
+*/
