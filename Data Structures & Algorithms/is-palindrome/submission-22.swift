dbbbb4dc3a6a56d6607414fcce4ffb76fa88e75f
Mahmoud Hamad <mahmoud_smgl@hotmail.com>
2026-09-26 13:06:00 +0300

    Add: is-palindrome - submission-22

---
+++ Data Structures & Algorithms/is-palindrome/submission-22.swift
@@ -0,0 +1,29 @@
+class Solution {
+    func isPalindrome(_ s: String) -> Bool {
+        var ar: [Character] = s.map { $0 }
+        let ignored: [Character] = [",","."]
+        var l = 0
+        var r = ar.count - 1
+        while l <= r {
+            if (ar[l].isLetter == false && ar[l].isNumber == false) || ignored.contains(ar[l]) {
+                print("ar[l]:\(ar[l])")
+                l += 1
+                continue
+            }
+            if (ar[r].isLetter == false && ar[l].isNumber == false) || ignored.contains(ar[r])  {
+                print("ar[r]:\(ar[r])")
+                r -= 1
+                continue
+            }
+            print("\(ar[l].lowercased()) == \(ar[r].lowercased())")
+            if ar[l].lowercased() == ar[r].lowercased() {
+                l += 1
+                r -= 1
+                continue
+            } else {
+                return false
+            }
+        }
+        return true
+    }
+}
